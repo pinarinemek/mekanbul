917a7399a0a0cf12acaed28e7950192b7de97fdb
pinarinemek <pinarinemek@gmail.com>
2022-11-10 19:53:06 +0300

    10kasimderssonu

---
+++ app_server/models/db.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+require("./mekansema");
+var dbURI = 'mongodb://localhost/mekanbul';
+mongoose.connect(dbURI);
+
+function kapat( msg , callback ){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+}
+
+process.on("SIGINT" , function(){
+    kapat("uygulama kapatıldı" , function(){
+        process.exit(0);
+    });
+});
+
+mongoose.connection.on('connected', function(){
+    console.log(dbURI + " adresindeki veritabanına bağlanıldı\n");
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log(dbURI + " adresindeki veritabanına bağlantısı koptu\n");
+});
+
+mongoose.connection.on('error', function() {
+    console.log("bağlantı hatası\n");
+});
+
